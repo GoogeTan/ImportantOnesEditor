9dfe0aef24008ab7f4d770a5c44d38bf5434c6ba
Katze <googletan@mail.ru>
2025-07-10 18:28:47 +0300

    Fix formatting

--- flake.nix
+++ flake.nix
@@ -1,34 +1,30 @@
 {
-  description = "A modular Emacs configuration based on Nix";
+    description = "A modular Emacs configuration based on Nix";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+        flake-utils.url = "github:numtide/flake-utils";
+        home-manager = {
+            url = "github:nix-community/home-manager";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
     };
-  };
 
-  outputs = { self, nixpkgs, flake-utils, home-manager }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-        };
+    outputs = { self, nixpkgs, flake-utils, home-manager }:
+        flake-utils.lib.eachDefaultSystem (system:
+            let
+                pkgs = import nixpkgs {
+                    inherit system;
+                    config.allowUnfree = true;
+                };
+                hmacs = pkgs.callPackage ./emacs { inherit home-manager; };
+            in {
+                packages.default = hmacs;
 
-        hmacs = pkgs.callPackage ./emacs { inherit home-manager; };
-
-      in
-      {
-        packages.default = hmacs;
-
-        apps.default = {
-          type = "app";
-          program = "${hmacs}/bin/emacs";
-        };
-      });
+                apps.default = {
+                    type = "app";
+                    program = "${hmacs}/bin/emacs";
+                };
+            }
+        );
 }
-  
-
